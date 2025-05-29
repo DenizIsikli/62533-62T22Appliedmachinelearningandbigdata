fc1a2e07f36a41a17bf4259fe35a977286b388d8
Deniz Isikli <s215818@dtu.dk>
2025-05-29 16:49:56 +0200

    download dataset refactor

--- Project/DatasetDownloader/download_dataset.py
+++ Project/DatasetDownloader/download_dataset.py
@@ -1,44 +1,49 @@
-import requests
 import os
+import requests
 from tqdm import tqdm
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-destination_path = os.path.join(script_dir, "OnlineRetail.xlsx")
+class DatasetDownloader:
+    def __init__(self):
+        self.script_dir = os.path.dirname(os.path.abspath(__file__))
+        self.destination_path = os.path.join(self.script_dir, "OnlineRetail.xlsx")
 
-def download_online_retail_data(destination=destination_path):
-    """This function downloads the Online Retail dataset from UCI Machine Learning Repository.
-    The dataset is an Excel file containing transactional data from a UK-based online retailer.
+        self.url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00352/Online%20Retail.xlsx"
+        self.download_online_retail_data()
 
-    Args:
-        destination (str): The path where the dataset will be saved. Default is "OnlineRetail.xlsx".
+    def download_online_retail_data(self):
+        """This function downloads the Online Retail dataset from UCI Machine Learning Repository.
+        The dataset is an Excel file containing transactional data from a UK-based online retailer.
 
-    Outputs:
-        - OnlineRetail.xlsx: The downloaded dataset file.
-    """
-    url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00352/Online%20Retail.xlsx"
-    print(f"Downloading dataset from {url}...")
+        Args:
+            destination (str): The path where the dataset will be saved. Default is "OnlineRetail.xlsx".
 
-    response = requests.get(url, stream=True)
-    total_size = int(response.headers.get('content-length', 0))
-    block_size = 1024  # 1 Kibibyte
+        Outputs:
+            - OnlineRetail.xlsx: The downloaded dataset file.
+        """
 
-    if response.status_code == 200:
-        with open(destination, "wb") as f, tqdm(
-            desc=os.path.basename(destination),
-            total=total_size,
-            unit='iB',
-            unit_scale=True,
-            unit_divisor=1024,
-        ) as bar:
-            for data in response.iter_content(block_size):
-                f.write(data)
-                bar.update(len(data))
-        print(f"\n Dataset saved as '{destination}'")
-    else:
-        print("Failed to download dataset. Please try manually.")
+        # check first if the destination file exists, if it does, skip the download
+        if os.path.exists(self.destination_path):
+            print(f"Dataset already exists at {self.destination_path}. Skipping download.")
+            return
 
-if __name__ == "__main__":
-    if not os.path.exists(destination_path):
-        download_online_retail_data()
-    else:
-        print("Dataset already exists.")
+        url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00352/Online%20Retail.xlsx"
+        print(f"Downloading dataset from {url}...")
+
+        response = requests.get(url, stream=True)
+        total_size = int(response.headers.get('content-length', 0))
+        block_size = 1024  # 1 Kibibyte
+
+        if response.status_code == 200:
+            with open(self.destination_path, "wb") as f, tqdm(
+                desc=os.path.basename(self.destination_path),
+                total=total_size,
+                unit='iB',
+                unit_scale=True,
+                unit_divisor=1024,
+            ) as bar:
+                for data in response.iter_content(block_size):
+                    f.write(data)
+                    bar.update(len(data))
+            print(f"\n Dataset saved as '{self.destination_path}'")
+        else:
+            print("Failed to download dataset. Please try manually.")
